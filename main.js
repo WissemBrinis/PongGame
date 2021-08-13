d8b3fc0c1386d424d03354c4d669547a395de07c
WissemBrinis <wissembenbrinis@gmail.com>
2021-08-13 18:18:33 +0100

    step4

--- main.js
+++ main.js
@@ -1,37 +1,69 @@
+var  $table=$('#table')
+var  $player1=$('#player')
+var  $player2=$('#player2')
 
+// console.log($table)
+// // player movement
+// $table.mousemove(function(evt){
+//   //  var  top= Math.min($table.height()-$player1.height),evt.pageY-$table.offset().top);
+//     // $player1.css({
+//     //     top: `${top}px`
+//     // })
+// })
+// // player2 movement 
+// $player2.mousemove(function(event){
+//     console.log(event)
+//     var  top= event.pageY;
+//     $player2.css({
+//         top: `${top}px`
+//     })
+// },83)
 
-var values = {
-    ball:{
-        speed:7,
-        x:50,
-        y:50,
-        directionX:1,
-        directionY:1,
-        radius:10
-    },
-    table:{
-        width : parseInt($('#table').width()),
-        height : parseInt($('#table').height())
-    },
-    player:{
-        y:0,
-       top: $("#player").offset().top,
-       height: $("#player").height(),
-       width: $("#player").width(),
-       score:0
-    },
-    player2:{
-       y:0,
-       top: $("#player2").offset().top,
-       height: $("#player2").height(),
-       width: $("#player2").width(),
-       score:0
+// ball movement
+// counter=0
+// document.addEventListener('keypress', logKey);
+var counter=0
+var counter1=0
+$('body').keypress(function(e){
+    console.log(e.keyCode)
+    
+    if(e.which === 51){
+        counter+=5
+        $player2.css({'margin-top':counter+'px'})
     }
-}
+    if(e.which === 57){ 
+         counter-=5
+        $player2.css({'margin-top':counter+'px'})
+    }
+})
 
-// collision detection
-function hitground(){
-    if(){
+$('body').keypress(function(e){
+    console.log(e.keyCode)
+    
+    if(e.which === 83){
+        counter1+=5
+        $player1.css({'margin-top':counter1+'px'})
+    }
+    if(e.which === 90){ 
+         counter1-=5
+        $player1.css({'margin-top':counter1+'px'})
+    }
+})
+
+
+// $('body').keypress(function(e){
+//     console.log(e.keyCode)
+//     if(e.which===14){
         
-    }
-}
+//     }
+// })
+
+
+
+
+
+  
+
+
+
+
