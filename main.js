3ca138a2c40309c6bd4cbfe9d88394ec788306da
WissemBrinis <wissembenbrinis@gmail.com>
2021-08-13 11:18:31 +0100

    step 3

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+var values = {
+    ball:{
+        speed:7,
+        x:50,
+        y:50,
+        directionX:1,
+        directionY:1,
+        radius:10
+    },
+    table:{
+        width : parseInt($('#table').width()),
+        height : parseInt($('#table').height())
+    },
+    player:{
+        y:0,
+       top: $("#player").offset().top,
+       height: $("#player").height(),
+       width: $("#player").width(),
+       score:0
+    },
+    player2:{
+       y:0,
+       top: $("#player2").offset().top,
+       height: $("#player2").height(),
+       width: $("#player2").width(),
+       score:0
+    }
+}
+
+// collision detection
+function hitground(){
+    if(){
+        
+    }
+}
